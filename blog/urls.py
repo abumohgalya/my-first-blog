07e50c77afcaabceda2e5c4b838e1a624a7ad3c2
abumohgalya <abumohgalya@gmail.com>
2018-06-20 02:11:01 +0300

    My Blog app, first commit

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    # url(r'^$', views.post_list, name='post_list'),
+    re_path('^$', views.post_list, name='post_list'),
+    re_path('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path('^post/new/$', views.post_new, name='post_new'),
+    re_path('^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    re_path('^drafts/$', views.post_draft_list, name='post_draft_list'),
+    re_path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    re_path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+]
